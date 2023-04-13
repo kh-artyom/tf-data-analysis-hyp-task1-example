be456480e513b96dc057763fc26a82845cb6af93
kh-artyom <129691199+kh-artyom@users.noreply.github.com>
2023-04-13 21:52:34 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,18 +1,12 @@
-import pandas as pd
+from scipy import stats
 import numpy as np
-from scipy.stats import norm
-from statsmodels.stats.proportion import proportions_ztest
 
 chat_id = 341299061 # Ваш chat ID, не меняйте название переменной
 
-def solution(x_success: int, 
-             x_cnt : int, 
-             y_success: int, 
-             y_cnt: int) -> bool:
-    alp = 0.02
-    z_stat, p_value = proportions_ztest([x_success, y_success], [x_cnt, y_cnt], value=0, alternative='smaller')
-    z_crit = np.abs(norm.ppf(alp))
-    if z_stat < -z_crit:
-        return True
-    else:
-        return False
+def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
+    p = (x_success + y_success) / (x_cnt + y_cnt)
+    p1 = x_success / x_cnt
+    p2 = y_success / y_cnt
+    z_stat = (p1 - p2) / np.sqrt(p * (1 - p) * ((1 / x_cnt) + (1 / y_cnt)))
+    p_value = stats.norm.sf(abs(z_stat)) * 2
+    return p_value < 0.02
